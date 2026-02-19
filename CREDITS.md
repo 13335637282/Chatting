bf20a490f099ec8f2c80b797b0004719e57c3f35
No_Bug <13335637282@163.com>
2026-02-19 16:58:32 +0800

    ui: 0.0.3 pyside UI

--- CREDITS.md
+++ CREDITS.md
@@ -7,11 +7,13 @@
 ### rich
 很好的美化输出工具
 ### textual
-虽然已经不是项目的UI框架了，但是他的 TUI 很美观
+虽然因为界面精度问题已经不是项目的UI框架了，但是他的 TUI 很美观
 ### rsa
 很好用的 RSA 加密库
 ### maliang
-很好用的 GUI 库，基于 tkinter
+很好用的 GUI 库，基于 tkinter，但目前文档不是很成熟所以现在已经不是项目 UI 框架了，他的 auto-zoom 很好用
+### pyside
+很好用的 GUI 库，基于 C QT 开发。
 
 ## 参与制作人员
 ### Cbscfe
--- client_ui.py
+++ client_ui.py
@@ -1,81 +1,123 @@
-from typing import List
+import pickle
+import sys
+from plistlib import load
 
-import maliang
-import rich.traceback
+from PySide6.QtCore import QFile, QIODevice
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import (QApplication, QCommandLinkButton, QLineEdit,
+                               QMainWindow, QMessageBox, QPushButton)
+from PySide6.scripts.metaobjectdump import Slot
 
-rich.traceback.install()
-
-from client_api import *
-
-logged_in = False
-token = ""
-input_pwd_count = 0
-pwds: List[str] = []
-
-root = maliang.Tk(title="登录")
-root.center()
-
-cv = maliang.Canvas(auto_zoom=True, keep_ratio="min", free_anchor=True)
-cv.place(width=1280, height=720, x=640, y=360, anchor="center")
-
-maliang.Text(cv, (640, 200), text="账 号 登 录", fontsize=48, anchor="center")
-
-maliang.Text(cv, (450, 300), text="账号", anchor="nw")
-user_name = maliang.InputBox(cv, (450, 340), (380, 50), placeholder="点击输入账号")
-maliang.Text(cv, (450, 400), text="密码", anchor="nw")
-password = maliang.InputBox(
-    cv, (450, 440), (380, 50), show="*", placeholder="点击输入密码"
-)
+from client_api import login, register
 
 
-def login_action():
-    r = login(user_name.get(), password.get())
-    if r.status_code == 200:
-        maliang.TkMessage("登录成功!")
-        global token
-
-        global logged_in
-        logged_in = True
-        root.destroy()
-        token = r.json().get("token")
-    elif r.status_code != 200 and r.status_code != -1:
-        maliang.TkMessage(f"登录失败: {r.json().get('error')}")
-    else:
-        maliang.TkMessage("发生了未知错误!")
+class RegLogWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.action = ""
+        self.logged_in = False
+        self.password = ""
+        self.user_name = ""
 
 
-def register_action():
-    global input_pwd_count
-    print(input_pwd_count, pwds, pwds.count(password.get()))
-    pwds.append(password.get())
-    if input_pwd_count >= 1 and pwds.count(password.get()) == 2:
-        r = register(user_name.get(), password.get())
-        if r.status_code == 201:
-            maliang.TkMessage("注册成功!")
-        elif r.status_code != 201 and r.status_code != -1:
-            maliang.TkMessage(f"注册失败: {r.json().get('error')}")
+class LoginWindow(RegLogWindow):
+    def __init__(self):
+        super().__init__()
+
+        loader = QUiLoader()
+        ui_file = QFile("login.ui")
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        self.login_button = self.window.findChild(QPushButton, "login")
+        self.login_button.clicked.connect(self.login)
+
+        self.register_button = self.window.findChild(QCommandLinkButton, "register_2")
+        self.register_button.clicked.connect(self.register)
+
+        self.input_user_name = self.window.findChild(QLineEdit, "user_name")
+        self.input_password = self.window.findChild(QLineEdit, "password")
+
+        self.logged_in = False
+        self.action = ""
+
+    def login(self):
+        r = login(self.input_user_name.text(), self.input_password.text())
+        if r.status_code == 200:
+            QMessageBox.information(None, "登录", "登录成功!")
+            self.logged_in = True
+            self.action = ""
+            self.user_name = self.input_user_name.text()
+            self.password = self.input_password.text()
+        elif r.status_code != 200 and r.status_code != -1:
+            try:
+                QMessageBox.warning(None, "登录", f"发生错误:{r.json()["error"]}")
+            except:
+                QMessageBox.warning(None, "登录", f"发生错误，但服务器没有提供错误信息")
         else:
-            maliang.TkMessage("发生了未知错误!")
-        password.clear()
-        input_pwd_count = 0
-    elif input_pwd_count < 1:
-        maliang.TkMessage("请再输入一遍密码")
-        password.clear()
-        input_pwd_count += 1
-    elif input_pwd_count >= 1 and pwds.count(password.get()) == 1:
-        maliang.TkMessage("第二次输入与第一次不匹配! 请重新输入密码2次")
-        input_pwd_count = 0
-        password.clear()
-        pwds.clear()
+            QMessageBox.warning(None, "登录", f"发生错误，无法跟服务器进行通讯")
+
+    def register(self):
+        self.action = "register"
+        self.window.close()
 
 
-maliang.Button(
-    cv, (450, 540), (180, 50), text="注 册", command=lambda: register_action()
-)
-maliang.Button(cv, (650, 540), (180, 50), text="登 录", command=lambda: login_action())
+class RegisterWindow(RegLogWindow):
+    def __init__(self):
+        super().__init__()
 
-root.mainloop()
-del root
-if logged_in:
-    root = maliang.Tk()
-    root.mainloop()
+        loader = QUiLoader()
+        ui_file = QFile("register.ui")
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        self.register_button = self.window.findChild(QPushButton, "register_2")
+        self.register_button.clicked.connect(self.register)
+
+        self.login_button = self.window.findChild(QCommandLinkButton, "login")
+        self.login_button.clicked.connect(self.login)
+
+        self.user_name = self.window.findChild(QLineEdit, "user_name")
+        self.password = self.window.findChild(QLineEdit, "password")
+        self.password_2 = self.window.findChild(QLineEdit, "password_2")
+
+        self.logged_in = False
+        self.action = ""
+
+    def register(self):
+        if self.password.text() == self.password_2.text():
+            r = register(self.user_name.text(), self.password.text())
+            if r.status_code == 201:
+                QMessageBox.information(None, "注册", "注册成功! 请登录!")
+                self.login()
+            elif r.status_code != 201 and r.status_code != -1:
+                try:
+                    QMessageBox.warning(None, "注册", f"发生错误:{r.json()["error"]}")
+                except:
+                    QMessageBox.warning(
+                        None, "注册", f"发生错误，但服务器没有提供错误信息"
+                    )
+            else:
+                QMessageBox.warning(None, "注册", f"发生错误，无法跟服务器进行通讯")
+        else:
+            QMessageBox.warning(None, "注册", "两次输入的密码不一致")
+
+    def login(self):
+        self.action = "login"
+        self.window.close()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    login_window: RegLogWindow = RegLogWindow()
+    login_window.action = "login"
+    while True:
+        if login_window.action == "register":
+            login_window = RegisterWindow()
+            login_window.window.show()  # type: ignore[attr-defined]
+        elif login_window.action == "login":
+            login_window = LoginWindow()
+            login_window.window.show()  # type: ignore[attr-defined]
+        elif login_window.logged_in:
+            break
+        app.exec()
